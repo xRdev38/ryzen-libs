77fa7b8a1433a536846e1a85ad9f0081bab17520
Rudy Nosile <rudy.nosile@educlever.com>
2023-09-26 17:37:59 +0200

    chore: rename directory

--- packages/web-api-ts/jest.config.ts
+++ packages/web-api-ts/jest.config.ts
@@ -6,5 +6,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/web-api',
+  coverageDirectory: '../../coverage/packages/web-api-ts',
 };
